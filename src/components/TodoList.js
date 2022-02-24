8a72d992d8e20ef29b06eddebf031562b1959694
Alan <m.limaalan@icloud.com>
2022-02-24 01:14:35 +0100

    Lint

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,7 +14,7 @@ function TodoList() {
 
   const completeTodo = id => {
     let updatedTodos = todos.map(todo => {
-      if (todo.id == id) {
+      if (todo.id === id) {
         todo.isComplete = !todo.isComplete
       }
       return todo;
